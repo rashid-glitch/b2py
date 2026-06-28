061f5c36f73260f2a4b35e8f3416cac8aac207ab
rashid-glitch <rashidfathuhudeen6@gmail.com>
2026-06-29 03:01:00 +0530

    Day 1 variables, arithmetics, string

---
+++ week 1/code.py
@@ -0,0 +1,7 @@
+fullName="Rashid Ray"
+i=0
+for char in fullName:
+  if char in "aeiouAEIOU":
+    i+=1
+print(i)
+print(fullName.split())
--- week 1/day1.py
+++ week 1/day1.py
@@ -1,5 +1,60 @@
+#Type Checking
+#--------------
+
 a="table"
 b=23
 c=4.2
 d=False
 print(type(a) ,type(b), type(c), type(d))
+
+#Swapping
+#---------------
+
+"""
+=10
+b=20
+print("Before swapping")
+print("a=", a)
+print("b=", b)
+temp=a
+a=b
+b=temp
+print("After swapping")
+print("a=", a)
+print("b=", b)
+"""
+a=10
+b=20
+print("before swapping")
+print(f"a= {a}, b={b}")
+a, b =b, a
+print("After swapping")
+print(f"a= {a}, b={b}")
+
+#Swapping
+#------------
+
+principal= 5000
+rate=8
+time =3
+si= (principal*rate*time)/100
+print("Simple interest=", si)
+
+#String Reverse and upper, lower
+#-----------
+
+fullName="Rashid Ray"
+print(fullName.upper())
+print(fullName.lower())
+print(fullName[::-1])
+
+#String split, check vowels
+#-------------
+
+fullName="Rashid Ray"
+i=0
+for char in fullName:
+  if char in "aeiouAEIOU":
+    i+=1
+print(i)
+print(fullName.split())
